92cc7cfc750664287f52d4fb3911215330801182
Matthias Geihs <matthias@tor.us>
2022-12-11 21:22:04 +0800

    sigma/dlog/encoder.go: Remove protocol type param

--- sigma/dlog/encoder.go
+++ sigma/dlog/encoder.go
@@ -7,32 +7,32 @@ import (
 	"github.com/matthiasgeihs/go-curve/sigma"
 )
 
-type Encoder[C curve.Curve, P sigma.Protocol] struct {
+type Encoder[C curve.Curve] struct {
 	gen curve.Generator[C]
 }
 
-func NewEncoder[C curve.Curve](gen curve.Generator[C]) Encoder[C, Protocol] {
-	return Encoder[C, Protocol]{
+func NewEncoder[C curve.Curve](gen curve.Generator[C]) Encoder[C] {
+	return Encoder[C]{
 		gen: gen,
 	}
 }
 
-func (e Encoder[C, P]) EncodeResponse(resp sigma.Response[C, P]) []byte {
+func (e Encoder[C]) EncodeResponse(resp sigma.Response[C, Protocol]) []byte {
 	dlogResp := resp.(Response[C])
 	return dlogResp.Int().Bytes()
 }
 
-func (e Encoder[C, P]) DecodeResponse(data []byte) sigma.Response[C, P] {
+func (e Encoder[C]) DecodeResponse(data []byte) sigma.Response[C, Protocol] {
 	bi := new(big.Int).SetBytes(data)
 	return Response[C](e.gen.NewScalar(bi))
 }
 
-func (e Encoder[C, P]) EncodeWitness(w sigma.Witness[C, P]) []byte {
+func (e Encoder[C]) EncodeWitness(w sigma.Witness[C, Protocol]) []byte {
 	dlogResp := w.(Response[C])
 	return dlogResp.Int().Bytes()
 }
 
-func (e Encoder[C, P]) DecodeWitness(data []byte) sigma.Witness[C, P] {
+func (e Encoder[C]) DecodeWitness(data []byte) sigma.Witness[C, Protocol] {
 	bi := new(big.Int).SetBytes(data)
 	return Witness[C](e.gen.NewScalar(bi))
 }
