4a4290309c0e97a30ba4b822fab6e3b64789a5b1
Matthias Geihs <matthias@tor.us>
2022-12-11 23:04:08 +0800

    verenc/cd00: update package documentation

---
+++ verenc/cd00/basic/doc.go
@@ -0,0 +1,4 @@
+// basic implements the basic verifiable encryption scheme from Camenisch and
+// Damgard, "Verifiable Encryption, Group Encryption, and Their Applications to
+// Separable Group Signatures and Signature Sharing Schemes", ASIACRYPT 2000.
+package basic
